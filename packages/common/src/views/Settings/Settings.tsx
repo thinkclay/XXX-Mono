f620484fb62a371f87fe9138e7c60628c2843eb7
Dhara12Kachhadia <dhara@silverscintilla.com>
2023-04-04 18:01:17 +0530

    Create settings page #9

---
+++ packages/common/src/views/Settings/Settings.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import { PageProps } from "@common/types/UI";
+
+function Settings({ mode }: PageProps) {
+  return (
+    <div className="Account auth centered copy">
+      <h1>Setting page</h1>
+    </div>
+  );
+}
+
+export default Settings;
