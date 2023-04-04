41e136c49e11e1a9ab7832ceb63b7785a5867280
Dhara12Kachhadia <dhara@silverscintilla.com>
2023-04-04 17:44:51 +0530

    Tone button logic and updates #8

--- packages/common/src/views/Revise/Scripe.tsx
+++ packages/common/src/views/Revise/Scripe.tsx
@@ -88,7 +88,7 @@ function Scribe({ editor, match, mode }: ScribeProps) {
         </div>
       )}
 
-      <Toolbar mode={mode} copy={_copy} reload={_reload} rewrite={_rewrite} />
+      <Toolbar mode={mode} copy={_copy} reload={_reload} rewrite={_rewrite} editor={editor}/>
     </div>
   )
 }
