3f447e1c8566145eba7e2674d351555cb3ad0712
Dhara12Kachhadia <dhara@silverscintilla.com>
2023-04-04 18:30:25 +0530

    add checkbox in setting page

--- packages/common/src/views/Settings/Settings.tsx
+++ packages/common/src/views/Settings/Settings.tsx
@@ -1,11 +1,26 @@
 /** @format */
 
 import { PageProps } from "@common/types/UI";
+import Fill from "../Welcome/Fill";
+import { useState } from "react";
 
 function Settings({ mode }: PageProps) {
+  const [_formState, _setFormState] = useState(false);
   return (
-    <div className="Account auth centered copy">
+    <div className="WelcomeScreen">
       <h1>Setting page</h1>
+      <ul className="terms">
+      <li onClick={() => _setFormState(!_formState)}>
+          <Fill checked={_formState} />
+          <input
+            type="checkbox"
+            name="spell-checker"
+            checked={_formState}
+            onChange={() => null}
+          />
+          <label htmlFor="spell-checker">Spell Checker</label>
+        </li>
+        </ul>
     </div>
   );
 }
