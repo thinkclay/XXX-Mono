799634631beb65863e0b801a84eeef812aeb5726
Dhara kachhadiya <dhara@silverscintilla.com>
2023-04-28 15:46:12 +0530

    add support for BehaviorReferral tab

--- packages/chrome/src/contents/infinite-campus.tsx
+++ packages/chrome/src/contents/infinite-campus.tsx
@@ -14,42 +14,52 @@ export const config: PlasmoCSConfig = {
   matches: ['*://nycloud1.infinitecampus.org/*'],
   run_at: 'document_start',
 }
+const PopUp = (() => {
+  return (
+    setTimeout(() => {
+      const workspaceDocumentHeader = document.getElementById('frameWorkspace')?.contentWindow.document.getElementById('frameWorkspaceWrapper')?.contentWindow.document.getElementById('frameWorkspaceHeader').contentDocument
+      const newButton = workspaceDocumentHeader?.getElementById('newDiv')
+      if (workspaceDocumentHeader && newButton) {
+        newButton?.addEventListener('click', () => {
+          setTimeout(() => {
+            const workspaceDocumentFooter = document.getElementById('frameWorkspace')?.contentWindow.document.getElementById('frameWorkspaceWrapper')?.contentWindow.document.getElementById('frameWorkspaceDetail')?.contentWindow.document.getElementById('detailFrame').contentDocument
+            if (workspaceDocumentFooter) {
+              const descriptionField = workspaceDocumentFooter?.getElementById('description');
+              if (descriptionField) {
+                console.log(descriptionField, workspaceDocumentFooter)
+                descriptionField?.addEventListener('click', () => {
+                  const updateHandler = (text: string) => {
+                    var newText = text.replace(/<\/?[^>]+>/gi, ' ');
+                    descriptionField.value = newText;
+                    const event = new Event('input', { bubbles: true });
+                    descriptionField.dispatchEvent(event);
+                  };
+                  runApp(document.body, updateHandler, descriptionField.value);
+                });
+              }
+            }
+          }, 1000)
+        })
 
+      }
+    }, 2000)
+  )
+})
 window.addEventListener('load', () => {
   console.log("infinitecampus")
   const iframeDocument = document.getElementById('frameSidebar')?.contentDocument
   if (iframeDocument) {
     setTimeout(() => {
-      const title = iframeDocument.querySelector('[title="behavior.BehaviorManagement"]');
-      if (title) {
-        title?.addEventListener('click', () => {
-          setTimeout(() => {
-            const workspaceDocumentHeader = document.getElementById('frameWorkspace')?.contentWindow.document.getElementById('frameWorkspaceWrapper')?.contentWindow.document.getElementById('frameWorkspaceHeader').contentDocument
-            const newButton = workspaceDocumentHeader?.getElementById('newDiv')
-            if (workspaceDocumentHeader) {
-              workspaceDocumentHeader?.addEventListener('click', () => {
-                setTimeout(() => {
-                  const workspaceDocumentFooter = document.getElementById('frameWorkspace')?.contentWindow.document.getElementById('frameWorkspaceWrapper')?.contentWindow.document.getElementById('frameWorkspaceDetail')?.contentWindow.document.getElementById('detailFrame').contentDocument
-                  if (workspaceDocumentFooter) {
-                    const descriptionField = workspaceDocumentFooter?.getElementById('description');
-                    if (descriptionField) {
-                      console.log(descriptionField, workspaceDocumentFooter)
-                      descriptionField?.addEventListener('click', () => {
-                        const updateHandler = (text: string) => {
-                          var newText = text.replace(/<\/?[^>]+>/gi, ' ');
-                          descriptionField.value = newText;
-                          const event = new Event('input', { bubbles: true });
-                          descriptionField.dispatchEvent(event);
-                        };
-                        runApp(document.body, updateHandler,descriptionField.value);
-                      });
-                    }
-                  }
-                }, 1000)
-              })
-
-            }
-          }, 2000)
+      const BehaviorManagement = iframeDocument.querySelector('[title="behavior.BehaviorManagement"]');
+      const BehaviorReferral = iframeDocument.querySelector('[title="behavior.BehaviorReferral"]');
+      if (BehaviorManagement) {
+        BehaviorManagement?.addEventListener('click', () => {
+          PopUp()
+        })
+      }
+      if (BehaviorReferral) {
+        BehaviorReferral?.addEventListener('click', () => {
+          PopUp()
         })
       }
     }, 2000)
@@ -72,7 +82,7 @@ function runApp(rootMount: Element, updateHandler: (text: string) => void, defau
     <RecoilRoot>
       <StrictMode>
         <div id="RevisionApp" className='infinite-campus'>
-          <MainScreen mode="embedded" onUpdate={updateHandler} defaultValue={defaultText}/>
+          <MainScreen mode="embedded" onUpdate={updateHandler} defaultValue={defaultText} />
           <Close handler={() => rootElement.remove()} />
           <div className="Overlay visible" onClick={() => rootElement.remove()}></div>
         </div>
