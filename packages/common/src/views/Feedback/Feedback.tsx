c59f79ad60547991da4cd5c794b70da52270b2b0
Dhara kachhadiya <dhara@silverscintilla.com>
2023-04-28 14:44:22 +0530

    modified logic for frame

--- packages/common/src/views/Feedback/Feedback.tsx
+++ packages/common/src/views/Feedback/Feedback.tsx
@@ -6,11 +6,10 @@ import { PageProps } from '@common/types/UI'
 function Feedback({ mode }: PageProps) {
   return (
     <div className="WelcomeScreen">
-      <h1>feedback page</h1>
       <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLScnBXnRyIGaM7gWoxORbR1ENOgjBDE78aveKm7cgfHCxpvDoQ/viewform?embedded=true"
+        src="https://docs.google.com/forms/d/e/1FAIpQLScBSAbu4sKwqZa4EAZQSpt2eUZie_jhA_9unXrsh2OuXLGbdA/viewform?embedded=true"
         width="100%"
-        height="800px"
+        height="1200px"
       >
         Loading…
       </iframe>
