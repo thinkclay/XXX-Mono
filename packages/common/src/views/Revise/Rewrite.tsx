bee7a73d50ca911e7adab9676b7b71c7679e0a38
Dhara12Kachhadia <dhara@silverscintilla.com>
2023-08-04 16:17:32 +0530

    store flag and accpectedflag in firebase with timpstamp

--- packages/common/src/views/Revise/Rewrite.tsx
+++ packages/common/src/views/Revise/Rewrite.tsx
@@ -1,10 +1,13 @@
 /** @format */
 
 import { useRecoilValue } from 'recoil'
-
 import { fetchingRevisionState } from '@common/helpers/root'
-import { ToolbarActionProps } from './Toolbar'
 import { biasCountState } from '@common/helpers/root'
+
+export interface ToolbarActionProps {
+  fetching?: boolean
+  handler: () => void
+}
 
 function Rewrite({ handler }: ToolbarActionProps) {
   const fetching = useRecoilValue(fetchingRevisionState)
@@ -13,12 +16,14 @@ function Rewrite({ handler }: ToolbarActionProps) {
   return (
     <button className={`rewrite ${fetching ? 'active fetching' : ''}`} onClick={handler}>
       {biasCount ? (
-        <div className="counter bias">{biasCount}</div>
+        <>
+          <div className="counter bias">{biasCount}</div>
+        </>
       ) : (
         <svg viewBox="0 0 70 68">
           <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
             <path
-              d="M1,60.8136887 C9.89707842,62.3145438 16.2023892,62.3145438 19.9159324,60.8136887 C35.51424,54.5095207 43.4024564,35.0144928 35,15.463237 C31.1772116,6.56818174 24.231007,6.15839994 21.3955329,7.43148889 C15.1542794,10.233726 15.1629649,18.4298629 18.1706655,27.2075445 C21.6566022,37.3809114 33.4746261,45.6710531 42.3913043,42.4026571 C56.5916882,37.1975262 63.6918801,26.1762313 63.6918801,9.33877235"
+              d="M1,60.8136887 Cs9.89707842,62.3145438 16.2023892,62.3145438 19.9159324,60.8136887 C35.51424,54.5095207 43.4024564,35.0144928 35,15.463237 C31.1772116,6.56818174 24.231007,6.15839994 21.3955329,7.43148889 C15.1542794,10.233726 15.1629649,18.4298629 18.1706655,27.2075445 C21.6566022,37.3809114 33.4746261,45.6710531 42.3913043,42.4026571 C56.5916882,37.1975262 63.6918801,26.1762313 63.6918801,9.33877235"
               className="StrokeLayer"
             ></path>
           </g>
